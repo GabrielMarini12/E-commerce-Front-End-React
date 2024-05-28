2c200e15551147f4b790fd3a82367a2c9ccbf4c2
Gabriel Marini <gabrielmarini12@icloud.com>
2024-05-28 19:08:07 -0300

    Ambiente preparado para desenvolvimento do E-commerce com React e Tailwindcss

---
+++ src/components/utils/Header.jsx
@@ -0,0 +1,13 @@
+import logo from "../../assets/logo/logo ecommerce.png";
+
+const Header = () => {
+  return (
+    <header>
+      <a href="/">
+        <img src={logo} alt="Logo da Zara" />
+      </a>
+    </header>
+  );
+};
+
+export default Header;
