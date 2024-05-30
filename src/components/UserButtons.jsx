7b89c6f668fa6749833bd46e9d8e71ef02641cad
Gabriel Marini <gabrielmarini12@icloud.com>
2024-05-30 18:01:37 -0300

    Realizado apenas o layout de quantos produtos temos no carrinho, aparece na bag do  header.

--- src/components/UserButtons.jsx
+++ src/components/UserButtons.jsx
@@ -4,8 +4,14 @@ import { faBagShopping, faUser } from "@fortawesome/free-solid-svg-icons";
 const UserButtons = () => {
   return (
     <div>
-      <button className="px-2">
+      <button className="px-2 relative">
         <FontAwesomeIcon icon={faBagShopping} />
+        <div
+          id="cart-amount"
+          className="absolute inline-flex items-center justify-center w-6 h-6 font-bold text-white bg-red-500 border border-white rounded-full -top-2 -right-2"
+        >
+          3
+        </div>
       </button>
       <a>
         <FontAwesomeIcon className="px-2" icon={faUser} />
