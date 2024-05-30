c83e803bd5bc7d90e8604b3dada8712b6fab5714
Gabriel Marini <gabrielmarini12@icloud.com>
2024-05-30 18:52:09 -0300

    Realizado um header dinâmico utilizando useLocation, que reage de acordo com a página que estou.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import logo from "../assets/logo/logo ecommerce.png";
 import UserButtons from "./UserButtons";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
-  return (
+  const { pathname } = useLocation();
+  const completeHeader = (
     <header className="flex sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-8 py-4 items-end justify-between text-xl z-10">
       <a href="/">
         <img src={logo} alt="Logo da Zara" className="h-20 px-2" />
@@ -10,6 +12,16 @@ const Header = () => {
       <UserButtons />
     </header>
   );
+
+  const simpleHeader = (
+    <header className="flex sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-8 py-4 items-end justify-between text-xl z-10">
+      <a href="/">
+        <img src={logo} alt="Logo da Zara" className="h-16 px-2" />
+      </a>
+    </header>
+  );
+
+  return <>{pathname === "/" ? completeHeader : simpleHeader} </>;
 };
 
 export default Header;
